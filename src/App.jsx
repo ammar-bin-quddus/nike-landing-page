033c7aae969a3434f579cf69a84fd5471f611aad
ammar-bin-quddus <ammarcghs372@gmail.com>
2024-08-15 13:29:09 +0600

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+const app = ()=> (
+  <main className="relative">
+    Nav
+    <section className="xl:padding-1 wide:padding-r padding-b">
+        hero
+    </section>
+    <section className="padding">
+        popularProducts
+    </section>
+    <section className="padding">
+        superQuality
+    </section>
+    <section className="padding-x py-10">
+        services
+    </section>
+    <section className="padding">
+      specialOffers
+    </section>
+    <section className="bg-pale-blue padding">
+      customerReviews
+    </section>
+    <section className="padding-x sm:py-32 py-16 w-full">
+      subscribe
+    </section>
+    <section className="bg-black padding-x padding-t pb-8">
+      footer
+    </section>
+
+  </main>
+);
