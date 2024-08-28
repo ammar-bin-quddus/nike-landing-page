186659923540a0e853e031030cd49a8d4070fd06
ammar-bin-quddus <ammarcghs372@gmail.com>
2024-08-28 15:23:30 +0600

    offer section complete

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,10 +1,19 @@
 
 
-const Button = ({ label, iconURL}) => {
+const Button = ({ label, iconURL, backgroundColor, borderColor, textColor}) => {
   return (
-    <button className="flex items-center gap-2 px-7 py-4 border font-montserrat leading-none text-lg bg-coral-red text-white rounded-full border-coral-red">
+    <button className={`flex justify-center items-center gap-2
+                        px-7 py-4 border font-montserrat text-lg leading-none
+                        ${backgroundColor ? 
+                        `${backgroundColor} ${borderColor} ${textColor}`: 
+                        "bg-coral-red text-white border-coral-red"} 
+                        rounded-full w-full"}`}>
+                       
+                        
         {label} 
-        <img src={iconURL} alt="image" className="ml-2 rounded-full w-5 h-5" />
+        { iconURL && <img src={iconURL} 
+             alt="image" 
+             className="ml-2 rounded-full w-5 h-5" /> }
     </button>
   )
 }
